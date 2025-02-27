d42a7ddb28f2e04eb08439515b0db9fe8533673a
Caroline Bertulucci <swnnex@gmail.com>
2025-02-26 21:57:01 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>teste</title>
+</head>
+<body>
+<form method="post" action="teste.php">
+  nome:  <input type="text" name="nome"><br>
+  idade: <input type="text" name="idade"><br>
+          <input type="submit" value="enviar">
+ </form>
+</body>
+</html>
---
+++ teste.php
@@ -0,0 +1,6 @@
+<?php
+echo "nome:".$_POST ['nome'].'<br>';
+echo "idade:".$_POST['idade'].'<br>';
+echo "<hr>";
+print_r ($_POST);
+?>
